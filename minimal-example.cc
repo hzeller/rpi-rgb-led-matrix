652d2526e1210914a77ffda28c6e61cc76eba78d
Henner Zeller <h.zeller@acm.org>
2014-09-06 22:52:27 +0000

    o make minimal example slightly less minimal.

--- minimal-example.cc
+++ minimal-example.cc
@@ -1,16 +1,38 @@
-// Small example how to use the libray
-
+// Small example how to use the library.
+// For more examples, look at demo-main.cc
+//
 #include "led-matrix.h"
 
 #include <unistd.h>
+#include <math.h>
 
 using rgb_matrix::GPIO;
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::Canvas;
 
+static void DrawOnCanvas(Canvas *canvas) {
+    /*
+     * Let's create a simple animation. We use the canvas to draw
+     * pixels. We wait between each step to have a slower animation.
+     */
+    canvas->FillScreen(0, 0, 255);
+
+    int center_x = canvas->width() / 2;
+    int center_y = canvas->height() / 2;
+    float radius_max = canvas->width() / 2;
+    float angle_step = 1.0 / 360;
+    for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
+        float dot_x = cos(a * 2 * M_PI) * r;
+        float dot_y = sin(a * 2 * M_PI) * r;
+        canvas->SetPixel(center_x + dot_x, center_y + dot_y,
+                         255, 0, 0);
+        usleep(1 * 1000);  // wait a little to slow down things.
+    }
+}
+
 int main(int argc, char *argv[]) {
     /*
-     * Set up GPIO pins. This fails when not running as root
+     * Set up GPIO pins. This fails when not running as root.
      */
     GPIO io;
     if (!io.Init())
@@ -23,23 +45,10 @@ int main(int argc, char *argv[]) {
     int chain = 1;   // Number of boards chained together.
     Canvas *canvas = new RGBMatrix(&io, rows, chain);
 
-    /*
-     * Now lets create a simple animation. We use the canvas to draw
-     * pixels. We wait between each step.
-     */
-    for (int x = 0; x < canvas->width(); ++x) {
-        for (int y = 0; y < canvas->height(); ++y) {
-            // Wander three dots around.
-            canvas->SetPixel(x, y, 255, 0, 0);
-            canvas->SetPixel(x, canvas->height() - y, 0, 255, 0);
-            canvas->SetPixel(y, x, 0, 255, 0);
-            usleep(15 * 1000);  // Wait for 15ms
-        }
-    }
-    
-    canvas->ClearScreen();
+    DrawOnCanvas(canvas);    // Using the canvas.
 
     // Animation finished. Shut down the RGB matrix.
+    canvas->ClearScreen();
     delete canvas;
 
     return 0;
