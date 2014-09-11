0124132650c75dea3b9030a6d5b0f92a39733eca
Henner Zeller <h.zeller@acm.org>
2014-09-11 08:17:46 +0000

    o fix datatype of codepoint.

--- lib/graphics.cc
+++ lib/graphics.cc
@@ -7,11 +7,11 @@ namespace rgb_matrix {
 int DrawText(Canvas *c, const Font &font,
              int x, int y, const Color &color,
              const char *utf8_text) {
-    const int start_x = x;
-    while (*utf8_text) {
-        uint8_t cp = utf8_next_codepoint(utf8_text);
-        x += font.DrawGlyph(c, x, y, color, cp);
-    }
-    return x - start_x;
+  const int start_x = x;
+  while (*utf8_text) {
+    const uint32_t cp = utf8_next_codepoint(utf8_text);
+    x += font.DrawGlyph(c, x, y, color, cp);
+  }
+  return x - start_x;
 }
 }
