dbf771c8874af922cdb66470e8ce41d8d6af7b4d
Saij <christoph@christophfriedrich.de>
2015-07-12 16:59:13 +0200

    o Added double buffering

--- python/samples/simple-square.py
+++ python/samples/simple-square.py
@@ -6,18 +6,20 @@ class SimpleSquare(SampleBase):
         super(SimpleSquare, self).__init__(*args, **kwargs)
 
     def Run(self):
+        offsetCanvas = self.matrix.CreateFrameCanvas()
         while True:
             for x in range(0, self.matrix.width):
-                self.matrix.SetPixel(x, x, 255, 255, 255)
-                self.matrix.SetPixel(self.matrix.height - 1 - x, x, 255, 0, 255)
+                offsetCanvas.SetPixel(x, x, 255, 255, 255)
+                offsetCanvas.SetPixel(offsetCanvas.height - 1 - x, x, 255, 0, 255)
 
-            for x in range(0, self.matrix.width):
-                self.matrix.SetPixel(x, 0, 255, 0, 0)
-                self.matrix.SetPixel(x, self.matrix.height - 1, 255, 255, 0)
+            for x in range(0, offsetCanvas.width):
+                offsetCanvas.SetPixel(x, 0, 255, 0, 0)
+                offsetCanvas.SetPixel(x, offsetCanvas.height - 1, 255, 255, 0)
 
-            for y in range(0, self.matrix.height):
-                self.matrix.SetPixel(0, y, 0, 0, 255)
-                self.matrix.SetPixel(self.matrix.width - 1, y, 0, 255, 0)
+            for y in range(0, offsetCanvas.height):
+                offsetCanvas.SetPixel(0, y, 0, 0, 255)
+                offsetCanvas.SetPixel(offsetCanvas.width - 1, y, 0, 255, 0)
+            offsetCanvas = self.matrix.SwapOnVSync(offsetCanvas)
 
 
 # Main function
