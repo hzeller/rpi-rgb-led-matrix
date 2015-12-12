dda4aa0edc73a58b0aab09b6939abc51dacf326e
Antoine VilleretÃ <antoine.villeret@gmail.com>
2015-12-12 10:46:09 +0000

    make example working with 8 chained displays

--- pixel-mapping.cc
+++ pixel-mapping.cc
@@ -14,6 +14,7 @@
 using rgb_matrix::GPIO;
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::Canvas;
+using namespace rgb_matrix;
 
 int main(int argc, char *argv[]) {
   /*
@@ -29,31 +30,14 @@ int main(int argc, char *argv[]) {
   int rows = 8;    // A 32x32 display. Use 16 when this is a 16x32 display.
   int chain = 16;    // Number of boards chained together.
   int parallel = 1; // Number of chains in parallel (1..3). > 1 for plus or Pi2
-  Canvas *canvas = new RGBMatrix(&io, rows, chain, parallel);
+  RGBMatrix *matrix = new RGBMatrix(&io, rows, chain, parallel);
+  Scrambled32x16Transformer *transformer = new Scrambled32x16Transformer();
+  matrix->SetTransformer(transformer);
 
-  int width=256, height=16;
-  int map[256][16];
-/*
-  for (int i=0; i<256*16; i++){
-    map[i%256][i/256] = i%8 + (2*(i/8)+1-(i%2048)/1024)*8%2048 + (i/2048)*2048;
-  }
-*/
-  for (int i=0; i<width*height; i++){
-    map[i%width][i/width] = i%8 + (2*(i/8)+1-(i%(8*width))/(4*width))*8%(8*width) + (i/(8*width))*(8*width);
-  }
-
-  printf("map table :\n");
-  for (int i=0; i<256; i++){
-    for(int j=0; j<16; j++){
-      printf("%d\t",map[i][j]);
-    }
-    printf("\n");
-  }
-
-  for (int j=0; j<16; j++){
-    for (int i=0; i<256; i++){
-      int idx = map[i][j];
-      canvas->SetPixel(idx%512,idx/512, 10, 10, 10);
+  printf("matrix dimen :  %d x %d\n", matrix->width(), matrix->height() );
+  for (int j=0; j<matrix->height(); j++){
+    for (int i=0; i<matrix->width(); i++){
+      matrix->SetPixel(i,j,10,10,10);
       usleep(10000);
     }
   }
@@ -61,8 +45,10 @@ int main(int argc, char *argv[]) {
   while(1);
 
   // Animation finished. Shut down the RGB matrix.
-  canvas->Clear();
-  delete canvas;
+  matrix->Clear();
+
+  delete transformer;
+  delete matrix;
 
   return 0;
 }
