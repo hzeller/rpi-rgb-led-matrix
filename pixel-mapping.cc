8914445e74cde8040c220c704df8bb3e9ac382ef
Antoine Villeret <antoine.villeret@gmail.com>
2015-12-12 11:10:18 +0100

    test code

--- pixel-mapping.cc
+++ pixel-mapping.cc
@@ -31,10 +31,15 @@ int main(int argc, char *argv[]) {
   int parallel = 1; // Number of chains in parallel (1..3). > 1 for plus or Pi2
   Canvas *canvas = new RGBMatrix(&io, rows, chain, parallel);
 
+  int width=256, height=16;
   int map[256][16];
-
+/*
   for (int i=0; i<256*16; i++){
     map[i%256][i/256] = i%8 + (2*(i/8)+1-(i%2048)/1024)*8%2048 + (i/2048)*2048;
+  }
+*/
+  for (int i=0; i<width*height; i++){
+    map[i%width][i/width] = i%8 + (2*(i/8)+1-(i%(8*width))/(4*width))*8%(8*width) + (i/(8*width))*(8*width);
   }
 
   printf("map table :\n");
