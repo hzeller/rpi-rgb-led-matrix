85c86675ec6e50ae68c74a7a692ac808f678bc30
Antoine VilleretÃ <antoine.villeret@gmail.com>
2016-03-14 13:43:15 +0100

    fix to allow several chains in parallel

--- lib/transformer.cc
+++ lib/transformer.cc
@@ -257,14 +257,14 @@ Striped4Multiplexing32x16Transformer::TransformCanvas::~TransformCanvas(){
 }
 
 void Striped4Multiplexing32x16Transformer::TransformCanvas::SetDelegatee(Canvas* delegatee) {
-  // The scrambled display I have has 2 chain per 32x16 panel, each of 8 rows
+  // The scrambled display I have has 2 chains per 32x16 panel, each of 8 rows
   // When we chain several of this scrambled display, the 8 lower rows of the entire display
   // is at the end of the chain.
   // So to use this display with the current library, we need to define 8 rows and 2 x number of diplay for chain number.
   // Here we set width and height in a more user friendly way.
-  assert(delegatee->height() == 8);
+
   assert(delegatee->width() % 32 == 0);
-  height_ = 16;
+  height_ = delegatee->height() * 2;
   width_ = delegatee->width()/2;
 
   if (!map_){
