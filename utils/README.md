cdc39384f9910f78d7180a9d0939b17affcdada8
Henner Zeller <h.zeller@acm.org>
2016-08-22 23:08:25 -0700

    o Provide a feature to go through a list of image files.   Fixes #126

--- utils/README.md
+++ utils/README.md
@@ -15,9 +15,41 @@ sudo apt-get install libgraphicsmagick++-dev libwebp-dev -y
 make led-image-viewer
 ```
 
+The resulting binary has a couple of flags.
+```
+usage: ./led-image-viewer [options] <image> [<image> ...]
+Options:
+        -w<seconds>               : If multiple images given: Wait time between in seconds (default: 1.5).
+        -f                        : Forever cycle through the list of files on the command line.
+        -t<seconds>               : For gif animations: stop after this time.
+        -l<loop-count>            : For gif animations: number of loops through a full cycle.
+
+General LED matrix options:
+        --led-rows=<rows>         : Panel rows. 8, 16, 32 or 64. (Default: 32).
+        --led-chain=<chained>     : Number of daisy-chained panels. (Default: 1).
+        --led-parallel=<parallel> : For A/B+ models or RPi2,3b: parallel chains. range=1..3 (Default: 1).
+   # more options for led matrix
+```
+
 Then, you can run it with any common image format, including animated gifs:
 
-    sudo ./led-image-viewer myimage.gif
+Examples:
+```bash
+sudo ./led-image-viewer some-image.jpg       # Display an image.
+sudo ./led-image-viewer animated.gif         # Show an animated gif
+sudo ./led-image-viewer -t5 animated.gif     # Show an animated gif for 5 seconds
+sudo ./led-image-viewer -l2 animated.gif     # Show an animated gif for 2 loops
+
+sudo ./led-image-viewer -w3 foo.jpg bar.png  # show two images, wait 3 seconds between. Stop.
+sudo ./led-image-viewer -f -w3 foo.gif bar.png  # show images, wait 3sec between, go back and loop forever
+
+sudo ./led-image-viewer -f -w3 *.png *.jpg   # Loop forever through a list of images
+
+# Show image.png and animated.gif in a loop. Show the static image for 3 seconds
+# while the animation is shown for 5 seconds (-t takes precendence for animated
+# images over -w)
+sudo ./led-image-viewer -f -w3 -t5 image.png animated.gif
+```
 
 It also supports the standard options to specify the connected
 displays (e.g. `--led-rows`, `--led-chain`, `--led-parallel` but also
--- utils/led-image-viewer.cc
+++ utils/led-image-viewer.cc
@@ -27,6 +27,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #include <vector>
 #include <Magick++.h>
@@ -43,6 +44,21 @@ static void InterruptHandler(int signo) {
   interrupt_received = true;
 }
 
+typedef int64_t tmillis_t;
+static tmillis_t GetTimeInMillis() {
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return tp.tv_sec * 1000 + tp.tv_usec / 1000;
+}
+
+static void SleepMillis(tmillis_t milli_seconds) {
+    if (milli_seconds <= 0) return;
+    struct timespec ts;
+    ts.tv_sec = milli_seconds / 1000;
+    ts.tv_nsec = (milli_seconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+}
+
 namespace {
 // Preprocess as much as possible, so that we can just exchange full frames
 // on VSync.
@@ -54,7 +70,7 @@ public:
     : canvas_(output) {
     int delay_time = img.animationDelay();  // in 1/100s of a second.
     if (delay_time < 1) delay_time = 1;
-    delay_micros_ = delay_time * 10000;
+    delay_millis_ = delay_time * 10;
 
     Canvas *const transformed_draw_canvas = transformer->Transform(output);
     for (size_t y = 0; y < img.rows(); ++y) {
@@ -73,160 +89,214 @@ public:
 
   FrameCanvas *canvas() const { return canvas_; }
 
-  int delay_micros() const {
-    return delay_micros_;
-  }
+  tmillis_t delay_millis() const { return delay_millis_; }
 
 private:
   FrameCanvas *const canvas_;
-  int delay_micros_;
+  tmillis_t delay_millis_;
 };
 }  // end anonymous namespace
 
+typedef std::vector<PreprocessedFrame*> PreprocessedList;
+
 // Load still image or animation.
-// Scale, so that it fits in "width" and "height" and store in "image_sequence".
-// If this is a still image, "image_sequence" will contain one image, otherwise
-// all animation frames.
-static bool LoadAnimation(const char *filename, int width, int height,
-                          std::vector<Magick::Image> *image_sequence) {
-  std::vector<Magick::Image> frames;
-  fprintf(stderr, "Read image...\n");
-  readImages(&frames, filename);
-  if (frames.size() == 0) {
-    fprintf(stderr, "No image found.");
-    return false;
-  }
-
-  // Put together the animation from single frames. GIFs can have nasty
-  // disposal modes, but they are handled nicely by coalesceImages()
-  if (frames.size() > 1) {
-    fprintf(stderr, "Assembling animation with %d frames.\n",
-            (int)frames.size());
-    Magick::coalesceImages(image_sequence, frames.begin(), frames.end());
-  } else {
-    image_sequence->push_back(frames[0]);   // just a single still image.
-  }
-
-  fprintf(stderr, "Scale ... %dx%d -> %dx%d\n",
-          (int)(*image_sequence)[0].columns(), (int)(*image_sequence)[0].rows(),
-          width, height);
-  for (size_t i = 0; i < image_sequence->size(); ++i) {
-    (*image_sequence)[i].scale(Magick::Geometry(width, height));
-  }
-  return true;
-}
-
-// Preprocess buffers: create readily filled frame-buffers that can be
-// swapped with the matrix to minimize computation time when we're displaying.
-static void PrepareBuffers(const std::vector<Magick::Image> &images,
-                           RGBMatrix *matrix,
-                           std::vector<PreprocessedFrame*> *frames) {
-  fprintf(stderr, "Preprocess for display.\n");
-  CanvasTransformer *const transformer = matrix->transformer();
-  for (size_t i = 0; i < images.size(); ++i) {
-    FrameCanvas *canvas = matrix->CreateFrameCanvas();
-    frames->push_back(new PreprocessedFrame(images[i], transformer, canvas));
-  }
-}
-
-static void DisplayAnimation(const std::vector<PreprocessedFrame*> &frames,
-                             RGBMatrix *matrix) {
-  signal(SIGTERM, InterruptHandler);
-  signal(SIGINT, InterruptHandler);
-  fprintf(stderr, "Display.\n");
-  for (unsigned int i = 0; !interrupt_received; ++i) {
-    const PreprocessedFrame *frame = frames[i % frames.size()];
-    matrix->SwapOnVSync(frame->canvas());
-    if (frames.size() == 1) {
-      sleep(86400);  // Only one image. Nothing to do.
-    } else {
-      usleep(frame->delay_micros());
+// Scale, so that it fits in "width" and "height" and store in "result".
+static bool LoadImageAndScale(const char *filename,
+                              int target_width, int target_height,
+                              bool fill_width, bool fill_height,
+                              std::vector<Magick::Image> *result) {
+    std::vector<Magick::Image> frames;
+    try {
+        readImages(&frames, filename);
+    } catch (std::exception& e) {
+        fprintf(stderr, "Trouble loading %s (%s)\n", filename, e.what());
+        return false;
     }
-  }
+    if (frames.size() == 0) {
+        fprintf(stderr, "No image found.");
+        return false;
+    }
+
+    // Put together the animation from single frames. GIFs can have nasty
+    // disposal modes, but they are handled nicely by coalesceImages()
+    if (frames.size() > 1) {
+        Magick::coalesceImages(result, frames.begin(), frames.end());
+    } else {
+        result->push_back(frames[0]);   // just a single still image.
+    }
+
+    const int img_width = (*result)[0].columns();
+    const int img_height = (*result)[0].rows();
+    const float width_fraction = (float)target_width / img_width;
+    const float height_fraction = (float)target_height / img_height;
+    if (fill_width && fill_height) {
+        // Scrolling diagonally. Fill as much as we can get in available space.
+        // Largest scale fraction determines that.
+        const float larger_fraction = (width_fraction > height_fraction)
+            ? width_fraction
+            : height_fraction;
+        target_width = (int) roundf(larger_fraction * img_width);
+        target_height = (int) roundf(larger_fraction * img_height);
+    }
+    else if (fill_height) {
+        // Horizontal scrolling: Make things fit in vertical space.
+        // While the height constraint stays the same, we can expand to full
+        // width as we scroll along that axis.
+        target_width = (int) roundf(height_fraction * img_width);
+    }
+    else if (fill_width) {
+        // dito, vertical. Make things fit in horizontal space.
+        target_height = (int) roundf(width_fraction * img_height);
+    }
+
+    for (size_t i = 0; i < result->size(); ++i) {
+        (*result)[i].scale(Magick::Geometry(target_width, target_height));
+    }
+
+    return true;
+}
+
+void DisplayAnimation(const PreprocessedList &frames,
+                      tmillis_t duration_ms, int loops, RGBMatrix *matrix) {
+    const tmillis_t end_time_ms = GetTimeInMillis() + duration_ms;
+    if (frames.size() == 1)
+        loops = 1;   // If there is no animation, nothing to repeat.
+    for (int k = 0;
+         (loops < 0 || k < loops)
+             && !interrupt_received
+             && GetTimeInMillis() < end_time_ms;
+         ++k) {
+        for (unsigned int i = 0; i < frames.size() && !interrupt_received; ++i) {
+            if (interrupt_received || GetTimeInMillis() > end_time_ms)
+                break;
+            PreprocessedFrame *frame = frames[i];
+            matrix->SwapOnVSync(frame->canvas());
+            SleepMillis(frames.size() == 1
+                        ? duration_ms : frame->delay_millis());
+        }
+    }
 }
 
 static int usage(const char *progname) {
-  fprintf(stderr, "usage: %s [options] <image>\n", progname);
-  fprintf(stderr, "Options:\n");
+  fprintf(stderr, "usage: %s [options] <image> [<image> ...]\n", progname);
+  fprintf(stderr, "Options:\n"
+          "\t-w<seconds>               : If multiple images given: "
+          "Wait time between in seconds (default: 1.5).\n"
+          "\t-f                        : "
+          "Forever cycle through the list of files on the command line.\n"
+          "\t-t<seconds>               : "
+          "For gif animations: stop after this time.\n"
+          "\t-l<loop-count>            : "
+          "For gif animations: number of loops through a full cycle.\n");
+
+  fprintf(stderr, "\nGeneral LED matrix options:\n");
   rgb_matrix::PrintMatrixFlags(stderr);
+
+  fprintf(stderr,
+          "Animated gifs                     : If both -l and -t are given, "
+          "whatever comes first stops.\n"
+          "Switch time between multiple files: "
+          "-w for static images; -t/-l for animations\n");
   return 1;
 }
-
-static bool WarnAboutDeprecatedOption(int argc, char **argv);
 
 int main(int argc, char *argv[]) {
   Magick::InitializeMagick(*argv);
   RGBMatrix *const matrix = rgb_matrix::CreateMatrixFromFlags(&argc, &argv);
 
-  if (WarnAboutDeprecatedOption(argc, argv))
-    return usage(argv[0]);
+  bool do_forever = false;
+  const tmillis_t distant_future = (1LL<<40); // that is a while.
+  tmillis_t anim_duration_ms = distant_future;
+  tmillis_t wait_ms = 1500;
+  int loops  = -1;
 
-  if (argc <= 1) {
+  int opt;
+  while ((opt = getopt(argc, argv, "w:t:l:fh")) != -1) {
+    switch (opt) {
+    case 'w':
+      wait_ms = roundf(atof(optarg) * 1000.0f);
+      break;
+    case 't':
+      anim_duration_ms = roundf(atof(optarg) * 1000.0f);
+      break;
+    case 'l':
+      loops = atoi(optarg);
+      break;
+    case 'f':
+      do_forever = true;
+      break;
+    case 'h':
+    default:
+      return usage(argv[0]);
+    }
+  }
+
+  const int filename_count = argc - optind;
+  if (filename_count == 0) {
     fprintf(stderr, "Expected image filename.\n");
     return usage(argv[0]);
+  }
+
+  if (filename_count == 1) {
+    wait_ms = distant_future;
+  }
+  else if (filename_count > 1 &&
+           loops < 0 && anim_duration_ms == distant_future) {
+    // More than one image but parameters for animations are default ? Set them
+    // to requested wait time, otherwise the first animation would just run
+    // forever.
+    anim_duration_ms = wait_ms;
   }
 
   if (matrix == NULL)
     return 1;
 
-  const char *filename = argv[argc-1];
+  const bool fill_width = false;
+  const bool fill_height = false;
 
-  std::vector<Magick::Image> sequence_pics;
-  if (!LoadAnimation(filename, matrix->width(), matrix->height(),
-                     &sequence_pics)) {
-    return 0;
+  // Preparing all the images beforehand as the Pi might be too slow to
+  // be quickly switching between these.
+  std::vector<PreprocessedList> file_imgs;
+  for (int imgarg = optind; imgarg < argc && !interrupt_received; ++imgarg) {
+      const char *filename = argv[imgarg];
+
+      std::vector<Magick::Image> image_sequence;
+      if (!LoadImageAndScale(filename, matrix->width(), matrix->height(),
+                             fill_width, fill_height, &image_sequence)) {
+        continue;
+      }
+
+      PreprocessedList frames;
+      CanvasTransformer *const transformer = matrix->transformer();
+      // Convert to preprocessed frames.
+      for (size_t i = 0; i < image_sequence.size(); ++i) {
+        FrameCanvas *canvas = matrix->CreateFrameCanvas();
+        frames.push_back(new PreprocessedFrame(image_sequence[i],
+                                               transformer, canvas));
+      }
+      file_imgs.push_back(frames);
   }
 
-  std::vector<PreprocessedFrame*> frames;
-  PrepareBuffers(sequence_pics, matrix, &frames);
+  signal(SIGTERM, InterruptHandler);
+  signal(SIGINT, InterruptHandler);
 
-  DisplayAnimation(frames, matrix);
+  do {
+    for (size_t i = 0; i < file_imgs.size() && !interrupt_received; ++i) {
+      const PreprocessedList frames = file_imgs[i];
 
-  fprintf(stderr, "Caught signal. Exiting.\n");
+      const tmillis_t duration = ((frames.size() == 1)
+                                  ? wait_ms
+                                  : anim_duration_ms);
+      DisplayAnimation(frames, duration , loops, matrix);
+    }
+  } while (do_forever && !interrupt_received);
+
+  if (interrupt_received)
+    fprintf(stderr, "Caught signal. Exiting.\n");
 
   // Animation finished. Shut down the RGB matrix.
   matrix->Clear();
   delete matrix;
 
   return 0;
-}
-
-static bool WarnAboutDeprecatedOption(int argc, char **argv) {
-    // These used to be options we understood, but deprecate now. Accept them
-  // for now, but tell the user.
-  bool any_deprecated_option = false;
-  int opt;
-  while ((opt = getopt(argc, argv, "r:P:c:p:b:d")) != -1) {
-    switch (opt) {
-    case 'r':
-      fprintf(stderr, "-r is a deprecated option. "
-              "Please use --led-rows=... instead!\n");
-      any_deprecated_option = true;
-      break;
-
-    case 'P':
-      fprintf(stderr, "-P is a deprecated option. "
-              "Please use --led-parallel=... instead!\n");
-      any_deprecated_option = true;
-      break;
-
-    case 'c':
-      fprintf(stderr, "-c is a deprecated option. "
-              "Please use --led-chain=... instead!\n");
-      any_deprecated_option = true;
-      break;
-
-    case 'p':
-      fprintf(stderr, "-p is a deprecated option. "
-              "Please use --led-pwm-bits=... instead!\n");
-      any_deprecated_option = true;
-      break;
-    case 'b':
-      fprintf(stderr, "-b is a deprecated option. "
-              "Please use --led-brightness=... instead!\n");
-      any_deprecated_option = true;
-      break;
-    }
-  }
-  return any_deprecated_option;
 }
