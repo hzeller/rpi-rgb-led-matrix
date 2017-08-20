cf735aa61786c089196780bed56b2d53764801ff
Christopher Amos <digital.synapse.software@gmail.com>
2017-08-20 16:23:45 -0700

    added minimal c# wrapper dll and c# example project

---
+++ c#/RGBLedMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpi_rgb_led_matrix_sharp
+{
+    public class RGBLedMatrixSharp
+    {
+        [DllImport("librgbmatrix.so")]
+        public static extern IntPtr led_matrix_create(int rows, int chained, int parallel);
+
+        [DllImport("librgbmatrix.so")]
+        public static extern void led_matrix_delete(IntPtr matrix);
+
+        [DllImport("librgbmatrix.so")]
+        public static extern void led_canvas_get_size(IntPtr canvas, out int width, out int height);
+
+        [DllImport("librgbmatrix.so")]
+        public static extern void led_canvas_set_pixel(IntPtr canvas, int x, int y, byte r, byte g, byte b);
+
+        [DllImport("librgbmatrix.so")]
+        public static extern void led_canvas_clear(IntPtr canvas);
+
+        [DllImport("librgbmatrix.so")]
+        public static extern void led_canvas_fill(IntPtr canvas, byte r, byte g, byte b);
+
+        [DllImport("librgbmatrix.so")]
+        public static extern IntPtr led_matrix_create_offscreen_canvas(IntPtr matrix);
+
+        [DllImport("librgbmatrix.so")]
+        public static extern IntPtr led_matrix_swap_on_vsync(IntPtr matrix, IntPtr canvas);
+    }
+}
---
+++ c#/testapp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rpi_rgb_led_matrix_sharp;
+
+namespace testapp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            var matrix= RGBLedMatrixSharp.led_matrix_create(32, 2, 1);
+            if (matrix == null) return 1;
+
+            var canvas = RGBLedMatrixSharp.led_matrix_create_offscreen_canvas(matrix);
+            int width;
+            int height;
+            RGBLedMatrixSharp.led_canvas_get_size(canvas, out width, out height);
+
+            for (var i = 0; i < 1000; ++i)
+            {
+                for (var y = 0; y < height; ++y)
+                {
+                    for (var x = 0; x < width; ++x)
+                    {
+                        RGBLedMatrixSharp.led_canvas_set_pixel(canvas, x, y, (byte)(i & 0xff), (byte)x, (byte)y);
+                    }
+                }
+                canvas = RGBLedMatrixSharp.led_matrix_swap_on_vsync(matrix, canvas);
+            }
+
+            RGBLedMatrixSharp.led_matrix_delete(matrix);
+            return 0; 
+        }
+    }
+}
