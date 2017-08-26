a24332863a5f8eb6c5fa44fa3fc5cfb59fd2875e
Christopher Amos <digital.synapse.software@gmail.com>
2017-08-26 15:05:38 -0700

    added bindings for font, circle, and line drawing. new wrapper class RGBLedFont added font-example.cs to examples folder

--- bindings/c#/RGBLedCanvas.cs
+++ bindings/c#/RGBLedCanvas.cs
@@ -20,7 +20,13 @@ namespace rpi_rgb_led_matrix_sharp
         internal static extern void led_canvas_clear(IntPtr canvas);
 
         [DllImport("librgbmatrix.so")]
-        internal static extern void led_canvas_fill(IntPtr canvas, byte r, byte g, byte b);       
+        internal static extern void led_canvas_fill(IntPtr canvas, byte r, byte g, byte b);
+
+        [DllImport("librgbmatrix.so")]
+        internal static extern void draw_circle(IntPtr canvas, int xx, int y, int radius, byte r, byte g, byte b);
+
+        [DllImport("librgbmatrix.so")]
+        internal static extern void draw_line(IntPtr canvas, int x0, int y0, int x1, int y1, byte r, byte g, byte b);
         #endregion
 
         // This is a wrapper for canvas no need to implement IDisposable here 
@@ -59,77 +65,18 @@ namespace rpi_rgb_led_matrix_sharp
 
         public void DrawCircle(int x0, int y0, int radius, Color color)
         {
-            int x = radius, y = 0;
-            int radiusError = 1 - x;
-
-            while (y <= x)
-            {
-                SetPixel(x + x0, y + y0, color);
-                SetPixel(y + x0, x + y0, color);
-                SetPixel(-x + x0, y + y0, color);
-                SetPixel(-y + x0, x + y0, color);
-                SetPixel(-x + x0, -y + y0, color);
-                SetPixel(-y + x0, -x + y0, color);
-                SetPixel(x + x0, -y + y0, color);
-                SetPixel(y + x0, -x + y0, color);
-                y++;
-                if (radiusError < 0)
-                {
-                    radiusError += 2 * y + 1;
-                }
-                else
-                {
-                    x--;
-                    radiusError += 2 * (y - x + 1);
-                }
-            }
+            draw_circle(_canvas, x0, y0, radius, color.R, color.G, color.B);
         }
 
         public void DrawLine (int x0, int y0, int x1, int y1, Color color)
         {
-            int dy = y1 - y0, dx = x1 - x0, gradient, x, y, shift = 0x10;
-
-            if (Math.Abs(dx) > Math.Abs(dy))
-            {
-                // x variation is bigger than y variation
-                if (x1 < x0)
-                {
-                    swap(ref x0, ref x1);
-                    swap(ref y0, ref y1);
-                }
-                gradient = (dy << shift) / dx;
-
-                for (x = x0, y = 0x8000 + (y0 << shift); x <= x1; ++x, y += gradient)
-                {
-                    SetPixel(x, y >> shift, color);
-                }
-            }
-            else if (dy != 0)
-            {
-                // y variation is bigger than x variation
-                if (y1 < y0)
-                {
-                    swap(ref x0, ref x1);
-                    swap(ref y0, ref y1);
-                }
-                gradient = (dx << shift) / dy;
-                for (y = y0, x = 0x8000 + (x0 << shift); y <= y1; ++y, x += gradient)
-                {
-                    SetPixel(x >> shift, y, color);
-                }
-            }
-            else
-            {
-                SetPixel(x0, y0, color);
-            }
+            draw_line(_canvas, x0, y0, x1, y1, color.R, color.G, color.B);
         }
 
-        private void swap(ref int a, ref int b)
+        public int DrawText(RGBLedFont font, int x, int y, Color color, string text, int spacing=0, bool vertical=false)
         {
-            int tmp = a;
-            a = b;
-            b = tmp;
-        }        
+            return font.DrawText(_canvas, x, y, color, text, spacing, vertical);
+        }
     }
 
     public struct Color
---
+++ bindings/c#/RGBLedFont.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpi_rgb_led_matrix_sharp
+{
+    public class RGBLedFont
+    {
+        [DllImport("librgbmatrix.so", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        internal static extern IntPtr load_font(string bdf_font_file);
+
+        [DllImport("librgbmatrix.so", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        internal static extern int draw_text(IntPtr canvas, IntPtr font, int x, int y, byte r, byte g, byte b, string utf8_text, int extra_spacing);
+
+        [DllImport("librgbmatrix.so", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        internal static extern int vertical_draw_text(IntPtr canvas, IntPtr font, int x, int y, byte r, byte g, byte b, string utf8_text, int kerning_offset);
+        
+        public RGBLedFont(string bdf_font_file_path)
+        {
+            _font = load_font(bdf_font_file_path);
+        }
+        internal IntPtr _font;
+
+        internal int DrawText(IntPtr canvas, int x, int y, Color color, string text, int spacing=0, bool vertical=false)
+        {
+            if (!vertical)
+                return draw_text(canvas, _font, x, y, color.R, color.G, color.B, text, spacing);
+            else
+                return vertical_draw_text(canvas, _font, x, y, color.R, color.G, color.B, text, spacing);
+        }
+
+    }
+}
