6e8813631a8d31ca02ad2ee107c38593751d9eb6
Dédé <maitredede@gmail.com>
2017-11-10 13:09:09 +1100

    Allow to change some options at runtime

--- include/led-matrix-c.h
+++ include/led-matrix-c.h
@@ -238,6 +238,8 @@ void draw_circle(struct LedCanvas *c, int xx, int y, int radius, uint8_t r, uint
 
 void draw_line(struct LedCanvas *c, int x0, int y0, int x1, int y1, uint8_t r, uint8_t g, uint8_t b);
 
+void led_matrix_update_options(const struct RGBLedMatrix *matrix, const struct RGBLedMatrixOptions *newOptions);
+
 #ifdef  __cplusplus
 }  // extern C
 #endif
