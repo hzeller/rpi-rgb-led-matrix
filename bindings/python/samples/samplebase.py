4063433a4b033ac886de09d1cf6c2e03c4421cdc
Álvaro Monares <alvaro@huilliche.cl>
2018-04-20 18:23:12 -0300

    Add a MultiplexMapper for a 32x16 1/2 scan panel brand Kaler, change the assert on framebuffer to allow a matrix of 4 rows. Changes on the python bindings to allow the new MultipleMapper on the examples. This MultiplexMapper works on scan_mode = 1 or 0  and row_address_type = 0

--- bindings/python/samples/samplebase.py
+++ bindings/python/samples/samplebase.py
@@ -26,7 +26,7 @@ class SampleBase(object):
         self.parser.add_argument("--led-rgb-sequence", action="store", help="Switch if your matrix has led colors swapped. Default: RGB", default="RGB", type=str)
         self.parser.add_argument("--led-pixel-mapper", action="store", help="Apply pixel mappers. e.g \"Rotate:90\"", default="", type=str)
         self.parser.add_argument("--led-row-addr-type", action="store", help="0 = default; 1=AB-addressed panels", default=0, type=int, choices=[0,1])
-        self.parser.add_argument("--led-multiplexing", action="store", help="Multiplexing type: 0=direct; 1=strip; 2=checker; 3=spiral; 4=ZStripe; 5=ZnMirrorZStripe; 6=coreman;7=Kaler2Scan (Default: 0)", default=0, type=int, choices=[0,1,2,3,4,5,6,7])
+        self.parser.add_argument("--led-multiplexing", action="store", help="Multiplexing type: 0=direct; 1=strip; 2=checker; 3=spiral; 4=ZStripe; 5=ZnMirrorZStripe; 6=coreman; 7=Kaler2Scan (Default: 0)", default=0, type=int, choices=[0,1,2,3,4,5,6,7])
 
     def usleep(self, value):
         time.sleep(value / 1000000.0)
