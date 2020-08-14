91702ff35e9da761fccb882eab1ab9718b3f4c08
Henner Zeller <h.zeller@acm.org>
2020-08-14 09:58:31 -0700

    Don't bother setting terminal in raw mode if it isn't a terminal.

--- examples-api-use/pixel-example.cc
+++ examples-api-use/pixel-example.cc
@@ -49,26 +49,32 @@ static bool parseColor(Color *c, const char *str) {
 }
 
 static char getch() {
-  struct termios old;
-  if (tcgetattr(0, &old) < 0)
-    perror("tcsetattr()");
+  static bool is_terminal = isatty(STDIN_FILENO);
 
-  // Set to unbuffered mode
-  struct termios no_echo = old;
-  no_echo.c_lflag &= ~ICANON;
-  no_echo.c_lflag &= ~ECHO;
-  no_echo.c_cc[VMIN] = 1;
-  no_echo.c_cc[VTIME] = 0;
-  if (tcsetattr(0, TCSANOW, &no_echo) < 0)
-    perror("tcsetattr ICANON");
+  struct termios old;
+  if (is_terminal) {
+    if (tcgetattr(0, &old) < 0)
+      perror("tcsetattr()");
+
+    // Set to unbuffered mode
+    struct termios no_echo = old;
+    no_echo.c_lflag &= ~ICANON;
+    no_echo.c_lflag &= ~ECHO;
+    no_echo.c_cc[VMIN] = 1;
+    no_echo.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &no_echo) < 0)
+      perror("tcsetattr ICANON");
+  }
 
   char buf = 0;
   if (read(0, &buf, 1) < 0)
     perror ("read()");
 
-  // Back to original terminal settings.
-  if (tcsetattr(0, TCSADRAIN, &old) < 0)
-    perror ("tcsetattr ~ICANON");
+  if (is_terminal) {
+    // Back to original terminal settings.
+    if (tcsetattr(0, TCSADRAIN, &old) < 0)
+      perror ("tcsetattr ~ICANON");
+  }
 
   return buf;
 }
