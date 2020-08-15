a550da95e56c2b62d3fc1f18ec95092143ad0623
Henner Zeller <h.zeller@acm.org>
2020-08-15 14:24:23 -0700

    Fix typo.

--- lib/multiplex-mappers.cc
+++ lib/multiplex-mappers.cc
@@ -109,7 +109,7 @@ public:
   FlippedStripeMultiplexMapper() : MultiplexMapperBase("FlippedStripe", 2) {}
 
   void MapSinglePanel(int x, int y, int *matrix_x, int *matrix_y) const {
-    const bool is_top_stripe = (y % (panel_rows_/2)) > panel_rows_/4;
+    const bool is_top_stripe = (y % (panel_rows_/2)) >= panel_rows_/4;
     *matrix_x = is_top_stripe ? x + panel_cols_ : x;
     *matrix_y = ((y / (panel_rows_/2)) * (panel_rows_/4)
                  + y % (panel_rows_/4));
