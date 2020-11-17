b52a1024733d72dba61665dbb44b148c098b7d7c
merliongold <58756554+merliongold@users.noreply.github.com>
2020-11-17 23:13:16 +0900

    Add files via upload
    Added DrawCircle with specific colored filling

--- include/graphics.h
+++ include/graphics.h
@@ -136,6 +136,11 @@ int VerticalDrawText(Canvas *c, const Font &font, int x, int y,
 // Draw a circle centered at "x", "y", with a radius of "radius" and with "color"
 void DrawCircle(Canvas *c, int x, int y, int radius, const Color &color);
 
+// Draw a circle centered at "x", "y", with a radius of "radius" and with "color"
+void DrawCircleFill(Canvas *c, int x, int y, int radius, const Color &color);
+void DrawFastVLine(Canvas *c, int x, int y, int radius, const Color &color);
+void FillCircleHelper(Canvas *c, int x, int y, int radius, int cornername, int delta, const Color &color);
+
 // Draw a line from "x0", "y0" to "x1", "y1" and with "color"
 void DrawLine(Canvas *c, int x0, int y0, int x1, int y1, const Color &color);
 
