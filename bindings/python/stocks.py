f3528585c9ffc77d7b6572e2da19b53969ad3fdb
andrewsiemer <andrew@siemer.org>
2022-12-15 15:40:25 -0800

    add stocks graph

--- bindings/python/stocks.py
+++ bindings/python/stocks.py
@@ -13,61 +13,129 @@ TICKER = "NVDA"
 
 class Market:
     def __init__(self):
-        self.latest = datetime.now(pytz.timezone("America/New_York"))
-        self.open = self.latest.replace(hour=9, minute=30, second=0, microsecond=0)
         self.exchange = "NYSE"
-        self.length = 390 # minutes in stock day
+        self.open_hour = 9
+        self.open_min = 30
+        self.open_time = 390 # minutes in stock day
         self.api_key = "d9142d27a31c40629d3e2347daa59c82"
+        self.timezone = "America/New_York"
+        self.update_interval = 6 # update every 6 mins when open
+        self.last_update = datetime.now() - timedelta(minutes=self.update_interval)
+        self.is_open = True
 
-    def updateDay(self):
-        if self.isOpen():
-            self.latest = datetime.now(pytz.timezone("America/New_York"))
-            self.open = self.latest.replace(hour=9, minute=30, second=0, microsecond=0)
-        elif (1):
-            pass
-
-    def isOpen(self):
+    def market_state(self):
         url = f"https://api.twelvedata.com/market_state?exchange={self.exchange}&apikey={self.api_key}"
         response = requests.get(url).json()
-        return response[0]['is_market_open']
+        if response[0]['is_market_open'] == 'true':
+            res = True
+        else:
+            res = False
+        return res
 
-    def getStockData(self, ticker):
-        url = f"https://api.twelvedata.com/time_series?symbol={ticker}&interval=1min&start_date={self.open}&end_date={self.latest}&apikey={self.api_key}"
+    def at_open(self, dt):
+        return dt.replace(hour=self.open_hour, minute=self.open_min, second=0, microsecond=0)
+
+    def get_time_series(self, ticker, start, end):
+        url = f"https://api.twelvedata.com/time_series?symbol={ticker}&interval=1day&start_date={start}&end_date={end}&apikey={self.api_key}"
+        return requests.get(url).json()
+
+    def update_status(self):
+        if (self.last_update + timedelta(minutes=self.update_interval) <= datetime.now()):
+            if self.is_open:
+                day = self.at_open(datetime.now(pytz.timezone(self.timezone)))
+                data = self.get_time_series("NVDA", day, day + timedelta(days=1))
+                while (data['status'] == 'error'):
+                    day -= timedelta(days=1)
+                    data = self.get_time_series("NVDA", day, day + timedelta(days=1))
+                self.trading_day = self.at_open(day)
+                
+                day -= timedelta(days=1)
+                data = self.get_time_series( "NVDA", day, day + timedelta(days=1))
+                while (data['status'] == 'error'):
+                    day -= timedelta(days=1)
+                    data = self.get_time_series("NVDA", day, day + timedelta(days=1))
+                self.previous_day = self.at_open(day)
+                
+                self.force_update = False
+                self.last_update = datetime.now()
+
+                print("current trading day:", self.trading_day)
+                print("previous trading day:", self.previous_day)
+
+            self.is_open = self.market_state()
+    
+    def get_last_close_price(self, ticker):
+        self.update_status()
+        url = f"https://api.twelvedata.com/time_series?symbol={ticker}&interval=1day&start_date={self.previous_day}&end_date={self.previous_day+timedelta(minutes=self.open_time)}&apikey={self.api_key}"
         response = requests.get(url).json()
-        print(response)
-        return response
+        return float(response['values'][0]['close'])
+    
+    def get_trading_day_data(self, ticker):
+        self.update_status()
+        url = f"https://api.twelvedata.com/time_series?symbol={ticker}&interval=1min&start_date={self.trading_day}&end_date={self.trading_day+timedelta(minutes=self.open_time)}&apikey={self.api_key}"
+        return requests.get(url).json()
 
 class Stocks(Base):
     def __init__(self, *args, **kwargs):
         super(Stocks, self).__init__(*args, **kwargs)
         self.graph = self.Graph()
+        self.market = Market()
 
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont("../../../fonts/7x13.bdf")
-        textColor = graphics.Color(255, 255, 0)
-        pos = offscreen_canvas.width
+        font.LoadFont("../../fonts/7x13.bdf")
+        white = graphics.Color(255, 255, 255)
+
+        self.graph.refresh(self.market.get_trading_day_data(TICKER),self.market.get_last_close_price(TICKER))
 
         while True:
             offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, "NVDA")
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
+            graphics.DrawText(offscreen_canvas, font, 1, 10, white, TICKER)
 
-            sleep(0.05)
+            x = 0
+            y_start = 31
+            green = (graphics.Color(0, 25, 0),graphics.Color(0, 255, 0))
+            red = (graphics.Color(25, 0, 0), graphics.Color(255, 0, 0))
+            for y in self.graph.y:
+                if y >= self.graph.inflection_pt:
+                    graphics.DrawLine(offscreen_canvas, x, y_start-y, x, y_start-self.graph.inflection_pt, green[0])
+                    if x == len(self.graph.y)-1:
+                        graphics.DrawLine(offscreen_canvas, x, y_start-y, x, y_start-y, green[1])
+                    else:
+                        next_y = y_start-self.graph.y[x+1]
+                        if next_y <= self.graph.inflection_pt:
+                            graphics.DrawLine(offscreen_canvas, x, self.graph.inflection_pt, x+1, next_y, red[1])
+                            graphics.DrawLine(offscreen_canvas, x, y_start-y, x+1, self.graph.inflection_pt, green[1])
+                        else:
+                            graphics.DrawLine(offscreen_canvas, x, y_start-y, x+1, next_y, green[1])
+                else:
+                    graphics.DrawLine(offscreen_canvas, x, y_start-y, x, y_start-self.graph.inflection_pt, red[0])
+                    if x == len(self.graph.y)-1:
+                        graphics.DrawLine(offscreen_canvas, x, y_start-y, x, y_start-y, red[1])
+                    else:
+                        next_y = y_start-self.graph.y[x+1]
+                        if next_y < self.graph.inflection_pt:
+                            graphics.DrawLine(offscreen_canvas, x, y_start-y, x+1, self.graph.inflection_pt, red[1])
+                            graphics.DrawLine(offscreen_canvas, x, self.graph.inflection_pt, x+1, next_y, green[1])
+                        else:
+                            graphics.DrawLine(offscreen_canvas, x, y_start-y, x+1, next_y, red[1])  
+
+                x += 1
+
+            sleep(1)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
     class Graph:
         def __init__(self):
             self.height = 17
             self.width = 64
-            self.length = 390 # minutes in stock day
-            self.timestamps = list(numpy.rint(numpy.linspace(0,self.length-1,self.width)))
+            self.open_time = 390 # minutes in stock day
+            self.timestamps = list(numpy.rint(numpy.linspace(0,self.open_time-1,self.width)))
             self.y = list()
+            self.x = list()
 
-        def refresh(self, data):
+        def refresh(self, data, close_price):
             samples = list()
             for delta in self.timestamps:
                 time = datetime.strptime(data['values'][-1]['datetime'], '%Y-%m-%d %H:%M:%S') + timedelta(minutes=delta)
@@ -82,24 +150,27 @@ class Stocks(Base):
 
             max_val = max(samples)
             min_val = min(samples)
+            
+            if close_price > max_val:
+                max_val = close_price
+            elif close_price < min_val:
+                min_val = close_price
+            
             scale = self.height/(max_val-min_val)
+            self.inflection_pt = int((close_price-min_val)*scale)
 
-            for x in samples:
-                self.y.append(int((x-min_val)*scale))
+            idx = 0
+            for sample in samples:
+                self.x.append(idx)
+                idx += 1
+
+                self.y.append(int((sample-min_val)*scale))
 
         def draw():
             pass
 
 # Main function
 if __name__ == "__main__":
-    market = Market()    
     stocks = Stocks()
 
-    print(market.isOpen())
-    data = market.getStockData(TICKER)
-    stocks.graph.refresh(data)
-
-    x = 0
-    for y in stocks.graph.y:
-        print(x, y, sep=', ')
-        x += 1
+    stocks.process()
