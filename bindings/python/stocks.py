4ab97bba66e5c5098d7ed32830b7e3864148709f
andrewsiemer <andrew@siemer.org>
2022-12-16 15:18:46 -0800

    add main app

--- bindings/python/stocks.py
+++ bindings/python/stocks.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from base import Base
 from rgbmatrix import graphics
 from time import sleep
 import json
@@ -10,7 +9,6 @@ from datetime import datetime, timedelta
 import pytz
 import numpy
 from twelvedata import TDClient
-import schedule
 
 import argparse
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
@@ -37,6 +35,7 @@ class Market:
 
             url = f"https://api.twelvedata.com/market_state?exchange={self.exchange}&apikey={self.api_key}"
             response = requests.get(url).json()
+            print("[STOCKS] URL:", url, "returned", response)
 
             if response[0]['is_market_open']:
                 self.is_open = True
@@ -109,7 +108,8 @@ class Market:
         )
         return ts.as_json()
 
-class Stocks(Base):
+market = Market()
+class Stocks:
     def __init__(self, matrix, symbol):
         self.graph = self.Graph()
         self.canvas = matrix.CreateFrameCanvas()
@@ -282,69 +282,3 @@ class Stocks(Base):
                             graphics.DrawLine(canvas, x+x_offset, self.inflection_pt, x+x_offset+1, next_y, green[1])
                         else:
                             graphics.DrawLine(canvas, x+x_offset, y_offset-y, x+x_offset+1, next_y, red[1])
-
-def handle_args(*args, **kwargs):
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-r", "--led-rows", action="store", help="Display rows. 16 for 16x32, 32 for 32x32. Default: 32", default=32, type=int)
-    parser.add_argument("--led-cols", action="store", help="Panel columns. Typically 32 or 64. (Default: 32)", default=64, type=int)
-    parser.add_argument("-c", "--led-chain", action="store", help="Daisy-chained boards. Default: 1.", default=1, type=int)
-    parser.add_argument("-P", "--led-parallel", action="store", help="For Plus-models or RPi2: parallel chains. 1..3. Default: 1", default=1, type=int)
-    parser.add_argument("-p", "--led-pwm-bits", action="store", help="Bits used for PWM. Something between 1..11. Default: 11", default=11, type=int)
-    parser.add_argument("-b", "--led-brightness", action="store", help="Sets brightness level. Default: 100. Range: 1..100", default=100, type=int)
-    parser.add_argument("-m", "--led-gpio-mapping", help="Hardware Mapping: regular, adafruit-hat, adafruit-hat-pwm", default='adafruit-hat-pwm', choices=['regular', 'regular-pi1', 'adafruit-hat', 'adafruit-hat-pwm'], type=str)
-    parser.add_argument("--led-scan-mode", action="store", help="Progressive or interlaced scan. 0 Progressive, 1 Interlaced (default)", default=1, choices=range(2), type=int)
-    parser.add_argument("--led-pwm-lsb-nanoseconds", action="store", help="Base time-unit for the on-time in the lowest significant bit in nanoseconds. Default: 130", default=130, type=int)
-    parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel")
-    parser.add_argument("--led-slowdown-gpio", action="store", help="Slow down writing to GPIO. Range: 0..4. Default: 1", default=1, type=int)
-    parser.add_argument("--led-no-hardware-pulse", action="store", help="Don't use hardware pin-pulse generation")
-    parser.add_argument("--led-rgb-sequence", action="store", help="Switch if your matrix has led colors swapped. Default: RGB", default="RGB", type=str)
-    parser.add_argument("--led-pixel-mapper", action="store", help="Apply pixel mappers. e.g \"Rotate:90\"", default="", type=str)
-    parser.add_argument("--led-row-addr-type", action="store", help="0 = default; 1=AB-addressed panels; 2=row direct; 3=ABC-addressed panels; 4 = ABC Shift + DE direct", default=0, type=int, choices=[0,1,2,3,4])
-    parser.add_argument("--led-multiplexing", action="store", help="Multiplexing type: 0=direct; 1=strip; 2=checker; 3=spiral; 4=ZStripe; 5=ZnMirrorZStripe; 6=coreman; 7=Kaler2Scan; 8=ZStripeUneven... (Default: 0)", default=0, type=int)
-    parser.add_argument("--led-panel-type", action="store", help="Needed to initialize special panels. Supported: 'FM6126A'", default="", type=str)
-    parser.add_argument("--led-no-drop-privs", dest="drop_privileges", help="Don't drop privileges from 'root' after initializing the hardware.", action='store_false')
-    parser.set_defaults(drop_privileges=True)
-
-    return parser.parse_args()
-
-def create_matrix(args):
-    options = RGBMatrixOptions()
-
-    options.hardware_mapping = args.led_gpio_mapping
-    options.rows = args.led_rows
-    options.cols = args.led_cols
-    options.chain_length = args.led_chain
-    options.parallel = args.led_parallel
-    options.row_address_type = args.led_row_addr_type
-    options.multiplexing = args.led_multiplexing
-    options.pwm_bits = args.led_pwm_bits
-    options.brightness = args.led_brightness
-    options.pwm_lsb_nanoseconds = args.led_pwm_lsb_nanoseconds
-    options.led_rgb_sequence = args.led_rgb_sequence
-    options.pixel_mapper_config = args.led_pixel_mapper
-    options.panel_type = args.led_panel_type
-
-    if args.led_show_refresh:
-        options.show_refresh_rate = 1
-
-    if args.led_slowdown_gpio != None:
-        options.gpio_slowdown = args.led_slowdown_gpio
-    if args.led_no_hardware_pulse:
-        options.disable_hardware_pulsing = True
-    if not args.drop_privileges:
-        options.drop_privileges=False
-
-    return RGBMatrix(options = options)
-
-market = Market()
-if __name__ == "__main__":
-    matrix = create_matrix(handle_args())
-
-    stocks = Stocks(matrix, "NVDA")
-
-    schedule.every(1).minutes.do(stocks.refresh)
-    while True:
-        schedule.run_pending()
-        sleep(5)
-        matrix.SwapOnVSync(stocks.get_canvas())
