28fd4d4ea344aec0f2642ef33d8249ff6afc3a5d
KirillAldashkin <a89086520092k@yandex.ru>
2023-05-29 21:48:01 +0800

    C#: Add [SuppressGCTransition] to trivial extern methods

--- bindings/c#/Bindings.cs
+++ bindings/c#/Bindings.cs
@@ -4,6 +4,14 @@ using System.Runtime.InteropServices;
 
 namespace RPiRgbLEDMatrix;
 
+/*
+Some of the extern methods listed below are marked with [SuppressGCTransition].
+This disables some GC checks that may take a long time. But such methods should
+be fast and trivial, otherwise the managed code may become unstable (see docs).
+Keep this in mind when changing the C/C++ side.
+
+https://learn.microsoft.com/dotnet/api/system.runtime.interopservices.suppressgctransitionattribute
+*/
 internal static class Bindings
 {
     private const string Lib = "librgbmatrix.so.1";
@@ -30,9 +38,11 @@ internal static class Bindings
     public static extern IntPtr led_matrix_get_canvas(IntPtr matrix);
 
     [DllImport(Lib)]
+    [SuppressGCTransition]
     public static extern byte led_matrix_get_brightness(IntPtr matrix);
 
     [DllImport(Lib)]
+    [SuppressGCTransition]
     public static extern void led_matrix_set_brightness(IntPtr matrix, byte brightness);
 
     [DllImport(Lib, CharSet = CharSet.Ansi)]
@@ -50,9 +60,11 @@ internal static class Bindings
     public static extern void delete_font(IntPtr font);
 
     [DllImport(Lib)]
+    [SuppressGCTransition]
     public static extern void led_canvas_get_size(IntPtr canvas, out int width, out int height);
 
     [DllImport(Lib)]
+    [SuppressGCTransition]
     public static extern void led_canvas_set_pixel(IntPtr canvas, int x, int y, byte r, byte g, byte b);
 
     [DllImport(Lib)]
