c7ed51decd0e91cd8f6b87b1b7214cd1bf843fe7
Matias Toro <matias.toro@flextime.ie>
2023-10-29 15:01:29 +0100

    wip

--- bindings/python/main/plain-text.py
+++ bindings/python/main/plain-text.py
@@ -9,18 +9,24 @@ class PlainText(CommonBase):
     def __init__(self, *args, **kwargs):
         super(PlainText, self).__init__(*args, **kwargs)
 
+    def getTextToShow(self):
+        text = ""
+        if self.args.text == True:
+            text = self.args.text
+            if self.args.centered :
+                text = self.args.text.center(self.args.padding)
+        elif self.args.clock == True:
+            text = time.strftime('%H:%M:%S')
+            if self.args.centered :
+                text = text.center(self.args.padding)
+
+        return text
     def run(self):
         self.args = self.parser.parse_args()
         canvas = self.matrix
         font = graphics.Font()
         font.LoadFont("../../../fonts/" + self.args.font)
-
         white = graphics.Color(255, 255, 255)
-        word = self.args.text
-        if self.args.centered :
-            word = word.center(self.args.padding)
-
-        mainModule.log("|" + word + "|")
         mainModule.log("Fade:" + str(self.args.fade))
 
         x = 0
@@ -30,32 +36,32 @@ class PlainText(CommonBase):
             y = -21
             while(y <= 21):
                 canvas.Clear()
-                graphics.DrawText(canvas, font, 0, y, white, word)
+                graphics.DrawText(canvas, font, 0, y, white, mainModule.getTextToShow())
                 time.sleep(0.200)
                 y = y + 2
         elif self.args.fade == 'bottom':
             y = 41
             while(y >= 21):
                 canvas.Clear()
-                graphics.DrawText(canvas, font, 0, y, white, word)
+                graphics.DrawText(canvas, font, 0, y, white, mainModule.getTextToShow())
                 time.sleep(0.200)
                 y = y - 2
         elif self.args.fade == 'left':
             x = -60
             while(x <= 0):
                 canvas.Clear()
-                graphics.DrawText(canvas, font, x, 21, white, word)
+                graphics.DrawText(canvas, font, x, 21, white, mainModule.getTextToShow())
                 time.sleep(0.200)
                 x = x + 2
         elif self.args.fade == 'right':
             x = 60
             while(x >= 0):
                 canvas.Clear()
-                graphics.DrawText(canvas, font, x, 21, white, word)
+                graphics.DrawText(canvas, font, x, 21, white, mainModule.getTextToShow())
                 time.sleep(0.200)
                 x = x - 2
         else:
-            graphics.DrawText(canvas, font, 0, 21, white, word)
+            graphics.DrawText(canvas, font, 0, 21, white, mainModule.getTextToShow())
 
         time.sleep(10)   # show display for 10 seconds before exit
 
