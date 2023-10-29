4d952b92d6467e91ae5f5de1e498b2e39e403d5c
Matias Toro <matias.toro@flextime.ie>
2023-10-29 14:16:25 +0100

    wip

--- bindings/python/main/gif-viewer.py
+++ bindings/python/main/gif-viewer.py
@@ -22,7 +22,7 @@ except Exception:
 # Configuration for the matrix
 options = RGBMatrixOptions()
 options.rows = 32
-options.cols = 32
+options.cols = 64
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
@@ -37,7 +37,8 @@ for frame_index in range(0, num_frames):
     gif.seek(frame_index)
     # must copy the frame out of the gif, since thumbnail() modifies the image in-place
     frame = gif.copy()
-    frame.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+    #frame.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+    frame.thumbnail((5, 5), Image.ANTIALIAS)
     canvas = matrix.CreateFrameCanvas()
     canvas.SetImage(frame.convert("RGB"))
     canvases.append(canvas)
