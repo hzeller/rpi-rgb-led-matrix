9a27bf59735bcf432927f1866f49918b3280b840
Matias Toro <matias.toro@flextime.ie>
2023-10-29 11:06:24 +0100

    wip

--- bindings/python/samples/plain-text.py
+++ bindings/python/samples/plain-text.py
@@ -12,27 +12,52 @@ class GraphicsTest(SampleBase):
         self.parser.add_argument("--padding", action="store", help="Padding of the word. Default: 32", default=10, type=int)
         self.parser.add_argument("--font", action="store", help="Font of the word. Default: 7x13.bdf", default="7x13.bdf", type=str)
         self.parser.add_argument("--centered", action="store", help="Center the word. Default: true", default="True", type=bool)
+        self.parser.add_argument("--fade", action="store", help="Animation Fade. Default: top", default="top", type=str)
 
     def run(self):
         self.args = self.parser.parse_args()
         canvas = self.matrix
         font = graphics.Font()
         font.LoadFont("../../../fonts/" + self.args.font)
-
-        # red = graphics.Color(255, 0, 0)
-        # graphics.DrawLine(canvas, 5, 5, 22, 13, red)
-
-        # green = graphics.Color(0, 255, 0)
-        # graphics.DrawCircle(canvas, 15, 15, 10, green)
-
-        #blue = graphics.Color(0, 0, 255)
         white = graphics.Color(255, 255, 255)
         word = self.args.text
         if self.args.centered :
             word = word.center(self.args.padding)
 
         print("|" + word + "|")
-        graphics.DrawText(canvas, font, 0, 21, white, word)
+
+        x = 0
+        y = 0
+
+        if self.args.padding == 'top':
+            y = -21
+            while(y <= 21):
+                graphics.DrawText(canvas, font, 0, y, white, word)
+                time.sleep(0.2)
+                y = y+1
+        elif self.args.padding == 'bottom':
+            y = 41
+            while(y >= 21):
+                graphics.DrawText(canvas, font, 0, y, white, word)
+                time.sleep(0.2)
+                y = y-1
+        elif self.args.padding == 'left':
+            x = -10
+            while(x <= 0):
+                graphics.DrawText(canvas, font, x, 21, white, word)
+                time.sleep(0.2)
+                x = x + 1
+        elif self.args.padding == 'right':
+            x = 20
+            while(x >= 0):
+                graphics.DrawText(canvas, font, x, 21, white, word)
+                time.sleep(0.2)
+                x = x - 1
+        else:
+            graphics.DrawText(canvas, font, 0, 21, white, word)
+
+
+
 
         time.sleep(20)   # show display for 10 seconds before exit
 
