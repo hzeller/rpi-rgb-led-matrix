492bbd566693420c9045f1d8553a108b9dcbd3ee
Matias Toro <matias.toro@flextime.ie>
2023-10-29 17:19:34 +0100

    WIP

--- bindings/python/main/main.py
+++ bindings/python/main/main.py
@@ -1,20 +1,85 @@
 #!/usr/bin/env python
 import random
+import math
 import sys
+import time
+
 from word import get_positive_word
 from common import CommonBase
 from rgbmatrix import graphics
-import time
-
 
 class PlainText(CommonBase):
     def __init__(self, *args, **kwargs):
         super(PlainText, self).__init__(*args, **kwargs)
 
+    def scale_col(val, lo, hi):
+        if val < lo:
+            return 0
+        if val > hi:
+            return 255
+        return 255 * (val - lo) / (hi - lo)
+
+
+    def rotate(x, y, sin, cos):
+        return x * cos - y * sin, x * sin + y * cos
+
     def prepare_word(self, word):
         if self.args.centered :
             word = word.center(self.args.padding)
         return word
+
+    def rotate_square(self):
+        cent_x = self.matrix.width / 2
+        cent_y = self.matrix.height / 2
+
+        rotate_square = min(self.matrix.width, self.matrix.height) * 1.41
+        min_rotate = cent_x - rotate_square / 2
+        max_rotate = cent_x + rotate_square / 2
+
+        display_square = min(self.matrix.width, self.matrix.height) * 0.7
+        min_display = cent_x - display_square / 2
+        max_display = cent_x + display_square / 2
+
+        deg_to_rad = 2 * 3.14159265 / 360
+        rotation = 0
+
+        # Pre calculate colors
+        col_table = []
+        for x in range(int(min_rotate), int(max_rotate)):
+            col_table.insert(x, mainModule.scale_col(x, min_display, max_display))
+
+        offset_canvas = self.matrix.CreateFrameCanvas()
+
+
+        while True:
+            rotation += 1
+
+            if rotation == 360:
+                return
+
+            rotation %= 360
+
+
+
+
+            # calculate sin and cos once for each frame
+            angle = rotation * deg_to_rad
+            sin = math.sin(angle)
+            cos = math.cos(angle)
+
+            for x in range(int(min_rotate), int(max_rotate)):
+                for y in range(int(min_rotate), int(max_rotate)):
+                    # Our rotate center is always offset by cent_x
+                    rot_x, rot_y = mainModule.rotate(x - cent_x, y - cent_x, sin, cos)
+
+                    if x >= min_display and x < max_display and y >= min_display and y < max_display:
+                        x_col = col_table[x]
+                        y_col = col_table[y]
+                        offset_canvas.SetPixel(rot_x + cent_x, rot_y + cent_y, x_col, 255 - y_col, y_col)
+                    else:
+                        offset_canvas.SetPixel(rot_x + cent_x, rot_y + cent_y, 0, 0, 0)
+
+            offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
     def show_text(self, word):
         canvas = self.matrix
@@ -75,10 +140,12 @@ class PlainText(CommonBase):
                     mainModule.show_text(word_selected);
                 elif action == 2: #Show Clock
                     word_selected = time.strftime('%H:%M')
-                    mainModule.log("antes:" + word_selected);
+                    mainModule.log("antes:" + word_selected)
                     word_selected = mainModule.prepare_word(word_selected)
-                    mainModule.log("despues:" + word_selected);
-                    mainModule.show_text(word_selected);
+                    mainModule.log("despues:" + word_selected)
+                    mainModule.show_text(word_selected)
+                elif action == 3: #rotate
+                    mainModule.rotate_square()
 
                 time.sleep(6)   # show display for 10 seconds before exit
 
