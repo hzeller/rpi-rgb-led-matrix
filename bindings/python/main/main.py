648277df71f314fd1ef2840314f9a803487f20ab
Matias Toro <matias.toro@flextime.ie>
2023-11-01 13:05:38 +0100

    Wip

--- bindings/python/main/main.py
+++ bindings/python/main/main.py
@@ -80,7 +80,7 @@ class PlainText(CommonBase):
         font = graphics.Font()
         font.LoadFont("../../../fonts/7x13.bdf")
 
-        kind = "Heavy rain at times"
+        kind = "Partly cloudy"
 
         image_weather_path = "../img/weather/" + kind + ".png"
 
