95edd44b2cb6118d7203eee9a5b653e093ccf5c2
Matias Toro <matias.toro@flextime.ie>
2023-11-07 17:53:36 +0100

    wip

--- src/main/commands.py
+++ src/main/commands.py
@@ -29,10 +29,10 @@ class Command:
             while(blink < 10):
                 blink = blink + 1
                 graphics.DrawText(canvas, font, 2, 19, white_color, "_")
-                graphics.DrawText(canvas, font, 5, 21, white_color, "dir")
+                graphics.DrawText(canvas, font, 12, 21, white_color, "dir")
                 time.sleep(1)
                 canvas.Clear()
-                graphics.DrawText(canvas, font, 5, 21, white_color, "dir")
+                graphics.DrawText(canvas, font, 12, 21, white_color, "dir")
                 time.sleep(1)
 
             time.sleep(5)
