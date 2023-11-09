7cbb88a517929eeb19b72669e6d37418b4ac790e
Matias Toro <matias.toro@flextime.ie>
2023-11-09 08:22:32 +0100

    wip

--- src/main/images.py
+++ src/main/images.py
@@ -2,6 +2,7 @@
 import time
 from glob import glob
 from random import choice
+import os
 
 from rgbmatrix import graphics
 from models.commands import get_command
@@ -12,6 +13,9 @@ class Images:
 
     def show_random(self):
         main_directory = "../img/fun"
-        random_file = choice(glob(f'{main_directory}/**/*.gif'))
+        #random_file = choice(glob(f'{main_directory}/**/*.gif'))
+
+        files = [i for i in glob.glob(f'{main_directory}/*/*') if os.path.isfile(i)]
+        random_file = random.choice(files)
         print(random_file)
         time.sleep(3)
