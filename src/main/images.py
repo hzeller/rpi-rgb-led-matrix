91352683c6cc16a1e7456ccf676240acaa790a9d
Matias Toro <matias.toro@flextime.ie>
2023-11-09 08:29:35 +0100

    wip

--- src/main/images.py
+++ src/main/images.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import time
-from glob import glob
-from random import choice
 import os
 
+from random import choice
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from models.commands import get_command
 from PIL import Image
+from glob import glob
 
 class Images:
     def get(self):
@@ -18,7 +19,19 @@ class Images:
         print(random_file)
 
         image = Image.open(random_file)
-        image.thumbnail((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
-        self.matrix.SetImage(image.convert('RGB'))
+
+        # Configuration for the matrix
+        options = RGBMatrixOptions()
+
+        options.rows = 32
+        options.cols = 64
+        options.chain_length = 1
+        options.parallel = 1
+        options.hardware_mapping = 'adafruit-hat'
+
+        matrix = RGBMatrix(options = options)
+
+        image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+        matrix.SetImage(image.convert('RGB'))
 
         time.sleep(5)
