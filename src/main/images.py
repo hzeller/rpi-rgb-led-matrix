236f6eed5394e6af45b116c3b32b45a23b363921
Matias Toro <matias.toro@flextime.ie>
2023-11-11 11:04:44 +0100

    Wip

--- src/main/images.py
+++ src/main/images.py
@@ -33,24 +33,35 @@ class Images:
         print(random_file)
 
         image = Image.open(random_file).convert('RGB')
+        img_width, img_height = image.size
+
+        print("self.matrix.width: " + str(self.matrix.width))
+        print("self.matrix.height: " + str(self.matrix.height))
+        print("img_width original: " + str(img_width))
+        print("img_height original: " + str(img_height))
 
         # image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
         image.resize((self.matrix.width, 25), Image.ANTIALIAS)
 
         double_buffer = self.matrix.CreateFrameCanvas()
         img_width, img_height = image.size
-
+        print("img_width despues: " + str(img_width))
+        print("img_height despues: " + str(img_height))
 
         xpos = 0
         times = 0
-        while times <= 3:
-            xpos += 1
-            if (xpos > img_width):
-                xpos = 0
-                times = times + 1
 
-            double_buffer.SetImage(image, -xpos)
-            double_buffer.SetImage(image, -xpos + img_width)
+        try:
+            while times <= 3:
+                xpos += 1
+                if (xpos > img_width):
+                    xpos = 0
+                    times = times + 1
 
-            double_buffer = self.matrix.SwapOnVSync(double_buffer)
-            time.sleep(0.01)
+                double_buffer.SetImage(image, -xpos)
+                double_buffer.SetImage(image, -xpos + img_width)
+
+                double_buffer = self.matrix.SwapOnVSync(double_buffer)
+                time.sleep(0.01)
+        except Exception as X:
+            print("Error in Images: " + str(X))
