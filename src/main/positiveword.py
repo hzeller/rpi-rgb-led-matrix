f811bb8487e50c69e2ae2b4e07377ee05292dda9
Matias Toro <matias.toro@flextime.ie>
2023-11-11 11:23:50 +0100

    Wip

--- src/main/positiveword.py
+++ src/main/positiveword.py
@@ -3,4 +3,7 @@ from models.words import get_positive_word
 
 class PositiveWords:
     def get(self):
-        return get_positive_word()
+        word = get_positive_word()
+        while len(word) <= 8:
+            word = get_positive_word()
+        return word
