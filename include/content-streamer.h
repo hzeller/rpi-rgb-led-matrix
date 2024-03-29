529c14ca9deafa47a8e1cfed9c3657a4a21a2fec
Hendrik Lind <34072808+sshcrack@users.noreply.github.com>
2024-03-29 15:06:16 +0100

    Added #pragma once for content-streamer.h

--- include/content-streamer.h
+++ include/content-streamer.h
@@ -11,6 +11,7 @@
 // These abstractions are used in util/led-image-viewer.cc to read and
 // write such animations to disk. It is also used in util/video-viewer.cc
 // to write a version to disk that then can be played with the led-image-viewer.
+#pragma once
 
 #include <stddef.h>
 #include <stdint.h>
