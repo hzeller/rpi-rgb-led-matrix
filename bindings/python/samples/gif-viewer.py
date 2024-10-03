f0eb30149250af46d07ec40b79da6188a5c3be68
Hannahcodesnow <162565119+Hannahcodesnow@users.noreply.github.com>
2024-10-03 13:12:58 +0200

    Update gif-viewer.py

--- bindings/python/samples/gif-viewer.py
+++ bindings/python/samples/gif-viewer.py
@@ -1,60 +1,55 @@
 #!/usr/bin/env python
 import time
-import sys
-
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from samplebase import SampleBase
 from PIL import Image
 
 
-if len(sys.argv) < 2:
-    sys.exit("Require a gif argument")
-else:
-    image_file = sys.argv[1]
+class GifViewer(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(GifViewer, self).__init__(*args, **kwargs)
+        self.parser.add_argument("-g", "--gif", help="The GIF file to display", required=True)
 
-gif = Image.open(image_file)
+    def run(self):
+        # Load the gif file
+        gif = Image.open(self.args.gif)
 
-try:
-    num_frames = gif.n_frames
-except Exception:
-    sys.exit("provided image is not a gif")
+        # Ensure the file is a GIF by checking frames
+        try:
+            num_frames = gif.n_frames
+        except Exception:
+            sys.exit("Provided image is not a gif")
 
+        # Preprocess the gif's frames into canvases to improve playback performance
+        canvases = []
+        print("Preprocessing gif, this may take a moment depending on the size of the gif...")
+        for frame_index in range(0, num_frames):
+            gif.seek(frame_index)
+            frame = gif.copy()  # Copy the current frame
+            frame.thumbnail((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+            canvas = self.matrix.CreateFrameCanvas()
+            canvas.SetImage(frame.convert("RGB"))
+            canvases.append(canvas)
 
-# Configuration for the matrix
-options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 32
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+        # Close the gif file to save memory
+        gif.close()
+        print("Completed preprocessing, displaying gif")
 
-matrix = RGBMatrix(options = options)
-
-# Preprocess the gifs frames into canvases to improve playback performance
-canvases = []
-print("Preprocessing gif, this may take a moment depending on the size of the gif...")
-for frame_index in range(0, num_frames):
-    gif.seek(frame_index)
-    # must copy the frame out of the gif, since thumbnail() modifies the image in-place
-    frame = gif.copy()
-    frame.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-    canvas = matrix.CreateFrameCanvas()
-    canvas.SetImage(frame.convert("RGB"))
-    canvases.append(canvas)
-# Close the gif file to save memory now that we have copied out all of the frames
-gif.close()
-
-print("Completed Preprocessing, displaying gif")
-
-try:
-    print("Press CTRL-C to stop.")
-
-    # Infinitely loop through the gif
-    cur_frame = 0
-    while(True):
-        matrix.SwapOnVSync(canvases[cur_frame], framerate_fraction=10)
-        if cur_frame == num_frames - 1:
+        try:
+            print("Press CTRL-C to stop.")
             cur_frame = 0
-        else:
-            cur_frame += 1
-except KeyboardInterrupt:
-    sys.exit(0)
+            # Loop infinitely through the gif frames
+            while True:
+                self.matrix.SwapOnVSync(canvases[cur_frame], framerate_fraction=10)
+                if cur_frame == num_frames - 1:
+                    cur_frame = 0
+                else:
+                    cur_frame += 1
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+
+# Main function
+if __name__ == "__main__":
+    gif_viewer = GifViewer()
+    if not gif_viewer.process():
+        gif_viewer.print_help()
