ba46d81f03865900bc1a43eb8e1ff299f423fa6d
Yaron M. Minsky <yminsky@gmail.com>
2025-02-02 15:46:16 -0500

    small manual fixes

--- bindings/ocaml/src/rgb_matrix_types.ml
+++
@@ -1,9 +0,0 @@
-open Ctypes
-
-type canvas
-type font 
-type matrix
-
-let canvas : canvas structure typ = structure "canvas"
-let font : font structure typ = structure "font"
-let matrix : matrix structure typ = structure "matrix"
