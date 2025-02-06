03cc0cc25194c03d0effff56cf6b82a3cd58d45d
John Heher <jheher@gmail.com>
2025-02-06 17:19:43 -0600

    Fixes simple square not staying on when running demo

--- examples-api-use/demo-main.cc
+++ examples-api-use/demo-main.cc
@@ -128,15 +128,18 @@ public:
   void Run() override {
     const int width = canvas()->width() - 1;
     const int height = canvas()->height() - 1;
-    // Borders
-    DrawLine(canvas(), 0, 0,      width, 0,      Color(255, 0, 0));
-    DrawLine(canvas(), 0, height, width, height, Color(255, 255, 0));
-    DrawLine(canvas(), 0, 0,      0,     height, Color(0, 0, 255));
-    DrawLine(canvas(), width, 0,  width, height, Color(0, 255, 0));
 
-    // Diagonals.
-    DrawLine(canvas(), 0, 0,        width, height, Color(255, 255, 255));
-    DrawLine(canvas(), 0, height, width, 0,        Color(255,   0, 255));
+    while (!interrupt_received) {
+      // Borders
+      DrawLine(canvas(), 0, 0,      width, 0,      Color(255, 0, 0));
+      DrawLine(canvas(), 0, height, width, height, Color(255, 255, 0));
+      DrawLine(canvas(), 0, 0,      0,     height, Color(0, 0, 255));
+      DrawLine(canvas(), width, 0,  width, height, Color(0, 255, 0));
+
+      // Diagonals.
+      DrawLine(canvas(), 0, 0,        width, height, Color(255, 255, 255));
+      DrawLine(canvas(), 0, height, width, 0,        Color(255,   0, 255));
+    }
   }
 };
 
