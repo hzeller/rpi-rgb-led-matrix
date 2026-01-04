7ccfb802030b23e4733c8e23e90c9bf10b3cb603
Clive Galway <evilc@evilc.com>
2026-01-04 05:37:33 +0000

    Add instructions for StreamPlayer

--- bindings/c#/examples/StreamPlayer/Program.cs
+++ bindings/c#/examples/StreamPlayer/Program.cs
@@ -9,7 +9,32 @@ if (args.Length > 0 && File.Exists(args[^1]))
 }
 else
 {
-    path = "/root/sample.stream"; // default path
+    path = "";
+    var defaultStreamPath = "/root/sample.stream";
+    Console.WriteLine("Usage: StreamPlayer [stream_path]\nWhere [stream_path] is the path to a stream file\n");
+    Console.WriteLine("Stream files can be created using the 'led-image-viewer' utility with the '-O' option.\n");
+    Console.WriteLine("Or type name of stream and hit ENTER");
+    Console.WriteLine($"Or just hit ENTER to use the default path: {defaultStreamPath}");
+    var enteredPath = Console.ReadLine();
+    while (path == "")
+    {
+        if (enteredPath != "")
+        {
+            if (File.Exists(enteredPath))
+            {
+                path = enteredPath;
+            }
+            else
+            {
+                Console.WriteLine($"File '{enteredPath}' does not exist. Please enter a valid file path or hit ENTER to use the default path.");
+                enteredPath = Console.ReadLine();
+            }
+        }
+        else
+        {
+            path = defaultStreamPath;
+        }
+    }
 }
 
 // Create an instance of StreamPlayer and play the file
